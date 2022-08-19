3402646c74e8a4113ff5d2f4aa058d9199a750b3
MrElectronium <46976544+MrElectronium@users.noreply.github.com>
2022-08-20 02:03:14 +0300

    Add files via upload

---
+++ EntropyAnalyzer/EntropyAnalyzer/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace EntropyAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea9 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend9 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(553, 328);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(284, 100);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 356);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Dosyayı Analiz et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea9.AxisX.Interval = 16D;
+            chartArea9.AxisX.MajorGrid.Enabled = false;
+            chartArea9.AxisX.Maximum = 256D;
+            chartArea9.AxisX.Minimum = 0D;
+            chartArea9.AxisY.Minimum = 0D;
+            chartArea9.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea9);
+            this.chart1.ImeMode = System.Windows.Forms.ImeMode.On;
+            legend9.Name = "Legend1";
+            this.chart1.Legends.Add(legend9);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series9.Legend = "Legend1";
+            series9.Name = "Bytes";
+            series9.SmartLabelStyle.Enabled = false;
+            series9.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt64;
+            series9.YValuesPerPoint = 2;
+            series9.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.UInt64;
+            this.chart1.Series.Add(series9);
+            this.chart1.Size = new System.Drawing.Size(1043, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 356);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Entropy:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(455, 324);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 23);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "16";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(379, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 43);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Hesapla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Dosya Boyutu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(111, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(243, 327);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(206, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Yüksek Frekanslı Sembol Adeti:\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 385);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 437);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "EntropyAnalyzer";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ EntropyAnalyzer/EntropyAnalyzer/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntropyAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        byte[] arr;
+        double[] ftbl = new double[256];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofn = new OpenFileDialog();
+            ofn.Filter = "All Files(*.*)|*.*";
+            if (ofn.ShowDialog() == DialogResult.OK)
+            {
+                arr=File.ReadAllBytes(ofn.FileName);
+                                
+                for (int i = 0; i < 256; i++)
+                    ftbl[i] = 0;
+                for (int i = 0; i < arr.Length; i++)
+                    ftbl[arr[i]] += 1;
+                chart1.Series[0].Points.Clear();
+                double len = arr.Length;
+                double num = 0;
+                double Entropy = 0;
+                for (int i = 0; i < 256; i++)
+                {
+                    chart1.Series[0].Points.AddXY(i,ftbl[i]);
+                    num = ftbl[i]/len;
+                    if (num == 0)
+                        num = 1 / (1 << 64);
+                    Entropy += -num * Math.Log(num, 2);
+                }
+                Entropy = Math.Round(Entropy, 7);
+                label2.Text = Entropy.ToString();
+                label4.Text = Convert.ToString(arr.Length);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (arr == null) return;
+            int coeff = 0;
+            coeff = Convert.ToInt32(textBox1.Text);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (byte)((int)arr[i] - coeff);
+            }
+            
+            ftbl = new double[256];
+            for (int i = 0; i < 256; i++)
+                ftbl[i] = 0;
+            for (int i = 0; i < arr.Length; i++)
+                ftbl[arr[i]] += 1;
+            chart1.Series[0].Points.Clear();
+            double len = arr.Length;
+            double num = 0;
+            double Entropy = 0;
+            listBox1.Items.Clear();
+            for (int i = 0; i < 256; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, ftbl[i]);
+                 num = ftbl[i] / len;                
+                if (num == 0)
+                    num = 1 / (1 << 64);
+                Entropy += -num * Math.Log(num, 2);
+            }
+            
+            Entropy = Math.Round(Entropy, 7);
+            label2.Text = Entropy.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (arr == null)
+            {
+                MessageBox.Show("Önce bir dosya açıp analiz yaptırmalısınız.","Hata");
+                return;
+            }
+            double max;
+            int maxi;
+            int cum_freq=0;
+            listBox1.Items.Clear();
+            string s="";
+            int maxfreq = 4;
+            maxfreq = Convert.ToInt32(textBox1.Text);
+            double[] ftbl2 = new double[256];
+            Array.Copy(ftbl, ftbl2, 256);
+            int clen;
+            for (int j = 0; j < maxfreq; j++)
+            {
+                max = 0;
+                maxi = 0;
+                s = "";
+                for (int i = 0; i < 256; i++)
+                {
+                 if(ftbl2[i]>max)
+                    {
+                        max = ftbl2[i];
+                        maxi = i;
+                    }    
+                }
+                s = String.Format("Sym:{0} Freq:{1}", maxi, max);
+                cum_freq += (int)max;
+                listBox1.Items.Add(s);
+                ftbl2[maxi] =0;
+            }
+            clen =(int)((double)cum_freq * (Math.Log((double)maxfreq, 2)+1) / 8 + (double)(arr.Length - cum_freq) * 9 / 8 + 256 + 20);
+            label5.Text = String.Format("Kümülatif Frekans:{0}"
+               , cum_freq);
+        }
+    }
+}
